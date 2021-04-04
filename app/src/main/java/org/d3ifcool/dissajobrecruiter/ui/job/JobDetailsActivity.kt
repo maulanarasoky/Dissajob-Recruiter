534c343f0fe9aab98ff2ce7c9a45a26d23486bc1
maulanarasoky <rasoky.maulana@gmail.com>
2021-04-05 00:24:43 +0700

    Finish implementing getApplicantsByJob function on JobDetailsActivity

--- app/src/main/java/org/d3ifcool/dissajobrecruiter/ui/job/JobDetailsActivity.kt
+++ app/src/main/java/org/d3ifcool/dissajobrecruiter/ui/job/JobDetailsActivity.kt
@@ -3,24 +3,30 @@ package org.d3ifcool.dissajobrecruiter.ui.job
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import org.d3ifcool.dissajobrecruiter.R
+import org.d3ifcool.dissajobrecruiter.data.source.local.entity.applicant.ApplicantDetailsEntity
 import org.d3ifcool.dissajobrecruiter.data.source.local.entity.job.JobDetailsEntity
 import org.d3ifcool.dissajobrecruiter.databinding.*
+import org.d3ifcool.dissajobrecruiter.ui.applicant.ApplicantAdapter
+import org.d3ifcool.dissajobrecruiter.ui.applicant.ApplicantViewModel
 import org.d3ifcool.dissajobrecruiter.ui.viewmodel.ViewModelFactory
 import org.d3ifcool.dissajobrecruiter.utils.DateUtils
 import org.d3ifcool.dissajobrecruiter.vo.Status
 
-class JobDetailsActivity : AppCompatActivity(), View.OnClickListener {
+class JobDetailsActivity : AppCompatActivity(), View.OnClickListener,
+    ApplicantAdapter.LoadApplicantDetailsCallback {
 
     companion object {
         const val EXTRA_ID = "extra_id"
     }
 
     private lateinit var activityJobDetailsBinding: ActivityJobDetailsBinding
+
+    private lateinit var viewModel: ApplicantViewModel
+
+    private lateinit var applicantAdapter: ApplicantAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,15 +42,15 @@ class JobDetailsActivity : AppCompatActivity(), View.OnClickListener {
             val jobId = extras.getString(EXTRA_ID)
             if (jobId != null) {
                 viewModel.getJobDetails(jobId).observe(this) { jobDetails ->
-                    when (jobDetails.status) {
-                        Status.LOADING -> {
-                        }
+                    if (jobDetails.data != null) {
+                        when (jobDetails.status) {
+                            Status.LOADING -> {
+                            }
 
-                        Status.SUCCESS -> jobDetails.data?.let {
-                            populateData(it)
-                        }
-                        Status.ERROR -> {
-                            Toast.makeText(this, "Error occurred", Toast.LENGTH_SHORT).show()
+                            Status.SUCCESS -> populateData(jobDetails.data)
+                            Status.ERROR -> {
+                                Toast.makeText(this, "Error occurred", Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
                 }
@@ -57,8 +63,13 @@ class JobDetailsActivity : AppCompatActivity(), View.OnClickListener {
         activityJobDetailsBinding.jobDetailsTitleSection.tvJobTitle.text =
             jobDetails.title.toString()
         val postedDate = DateUtils.getPostedDate(jobDetails.postedDate.toString())
+        getApplicantsByJob(jobDetails.id)
         activityJobDetailsBinding.jobDetailsTitleSection.tvJobPostedDateAndApplicants.text =
-            resources.getString(R.string.job_details_date_format, postedDate, 0)
+            resources.getString(
+                R.string.job_details_date_format,
+                postedDate,
+                applicantAdapter.itemCount
+            )
 
         //Description section
         activityJobDetailsBinding.jobDetailsDescriptionSection.tvDescription.text =
@@ -75,9 +86,49 @@ class JobDetailsActivity : AppCompatActivity(), View.OnClickListener {
             jobDetails.salary.toString()
     }
 
+    private fun getApplicantsByJob(jobId: String) {
+        val factory = ViewModelFactory.getInstance(this)
+        val viewModel = ViewModelProvider(this, factory)[ApplicantViewModel::class.java]
+        applicantAdapter = ApplicantAdapter(this)
+        viewModel.getApplicantsByJob(jobId).observe(this) { applicants ->
+            if (applicants.data != null) {
+                when (applicants.status) {
+                    Status.LOADING -> {
+                    }
+                    Status.SUCCESS -> {
+                        if (applicants.data.isNotEmpty()) {
+                            applicantAdapter.submitList(applicants.data)
+                            applicantAdapter.notifyDataSetChanged()
+                        } else {
+                            activityJobDetailsBinding.jobDetailsApplicantsSection.tvNoData.visibility =
+                                View.VISIBLE
+                        }
+                    }
+                    Status.ERROR -> {
+                        Toast.makeText(this, "Error occurred", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.imgBackBtn -> finish()
         }
+    }
+
+    override fun onLoadApplicantDetailsCallback(
+        applicantId: String,
+        callback: ApplicantAdapter.LoadApplicantDetailsCallback
+    ) {
+        viewModel.getApplicantDetails(applicantId).observe(this) { applicantDetails ->
+            if (applicantDetails.data != null) {
+                callback.onGetApplicantDetails(applicantDetails.data)
+            }
+        }
+    }
+
+    override fun onGetApplicantDetails(applicantDetails: ApplicantDetailsEntity) {
     }
 }
